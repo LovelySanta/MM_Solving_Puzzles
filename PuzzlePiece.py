6ec96c420468689a50d7c44f362615e080403d01
LovelySanta <laurens.scheldeman@gmail.com>
2017-11-21 15:48:02 +0100

    Added rotate methode

--- PuzzlePiece.py
+++ PuzzlePiece.py
@@ -3,15 +3,15 @@ import cv2
 
 class PuzzlePiece:
 
-    def __init__(self, original_piece, rotated_piece):
-        self.piece = original_piece
-        self.rotatedPiece = rotated_piece
+    def __init__(self, piece):
+        self.piece = piece
 
         return None
 
     def showPuzzlePiece(self):
         cv2.imshow('Original Piece', self.piece)
-        cv2.imshow('Rotated Piece', self.rotatedPiece)
+        #cv2.imshow('Rotated Piece', self.rotatedPiece)
         cv2.waitKey()
         cv2.destroyAllWindows()
+
 ### End Of File ###
