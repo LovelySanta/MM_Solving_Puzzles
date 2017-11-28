5a469159049099e7790355883134a96b1c353fa0
LovelySanta <laurens.scheldeman@gmail.com>
2017-11-28 15:28:41 +0100

    Implemented cutPuzzlePiecesOut()

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ puzzle = Puzzle()
 puzzle.readImageFromFile('images/'+puzzleType+'/'+puzzleType+'_'+puzzleArrangement+'/'+puzzleType+'_'+puzzleArrangement+'_'+puzzleSize+'_'+puzzleNumber+'.png')
 
 ### Create the mask to get pieces out of the background
-puzzle.createMask()
-puzzle.showImage(windowName="original", addWaitKey=False)
+#puzzle.createMask()
+#puzzle.showImage(windowName="original", addWaitKey=False)
 
 ### Now we got the mask, let's get the pieces of the puzzle out
-puzzle.findPuzzlePieces()
-puzzle.cutAllPiecesOut()
+#puzzle.findPuzzlePieces()
+#puzzle.cutAllPiecesOut()
 
+#puzzle.showAllPieces()
+
+
+puzzle.cutPuzzlePieces(4)
 puzzle.showAllPieces()
