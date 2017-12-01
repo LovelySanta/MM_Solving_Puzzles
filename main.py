091e012bf644ebe5d589344598e16ee51a9c371f
LaurensScheldeman <laurens.scheldeman@gmail.com>
2017-12-02 00:47:10 +0100

    Made the main program readable

--- main.py
+++ main.py
@@ -2,23 +2,42 @@
 from Puzzle import Puzzle
 
 
-puzzleType="jigsaw"
-puzzleArrangement="rotated"
+puzzleType="tiles"
+puzzleArrangement="shuffled"
 puzzleSize="5x5"
 puzzleNumber="04"
 
+
+
+
+### STEP 1: READ THE PUZZLE INPUT and determine settings
 puzzle = Puzzle()
 puzzle.readImageFromFile('images/'+puzzleType+'/'+puzzleType+'_'+puzzleArrangement+'/'+puzzleType+'_'+puzzleArrangement+'_'+puzzleSize+'_'+puzzleNumber+'.png')
 
-## Create the mask to get pieces out of the background
-puzzle.createMask()
-puzzle.showImage(windowName="original", addWaitKey=False)
+#TODO: determine settings
+puzzleHasBackground = False
+puzzlePiecesAmount = 25 # Only needed if not with background
+puzzleHasJigSaw = False
 
-## Now we got the mask, let's get the pieces of the puzzle out
-puzzle.findPuzzlePieces()
-puzzle.cutAllPiecesOut()
 
-puzzle.showAllPieces()
+### STEP 2: CUT OUT ALL THE PIECES
+if puzzleHasBackground: ### Puzzle with background
+    ## Create the mask to get pieces out of the background
+    puzzle.createMask()
+    puzzle.showImage(windowName="original", addWaitKey=False)
+
+    ## Now we got the mask, let's get the pieces of the puzzle out
+    puzzle.findPuzzlePieces()
+    puzzle.cutAllPiecesOut()
+
+    puzzle.showAllPieces()
+
+else: ### Puzzle without background
+    puzzle.cutPuzzlePieces(puzzlePiecesAmount)
+
+    #puzzle.showAllPieces()
+
+
 
 
 #puzzle.cutPuzzlePieces(4)
