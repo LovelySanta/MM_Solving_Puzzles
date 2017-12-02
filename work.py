1e3721a6952531100fa2b79002588021e8a73605
Jarno21 <Jagrenson@gmail.com>
2017-12-02 11:04:50 +0100

    bugfix
    overflowing matrix solved

--- work.py
+++ work.py
@@ -33,38 +33,44 @@ def mirror(image,op):
 def getMatch(e1, e2):
     dif = len(e1)-len(e2)
     if abs(dif) > 5:
-        return 10000
+        return 50000
     elif dif == 0:
         #cv2.imshow('diff', abs(cv2.absdiff(e2, e1))[:,0])
         
-        return sum(sum(abs(cv2.absdiff(e2, e1)*1.0)))*256.0/len(e1)
+        return sum(sum(abs(cv2.absdiff(e2, e1)*0.1)))*100.0/len(e1)
     elif dif > 0:
-        match = 10000*len(e2)
+        match = 50000
         for i in range(dif):
-            match = min(match, sum(sum(cv2.absdiff(e2, e1[i:i+len(e2)])*1.0)))
-        return match*1.0/len(e2)
+            match = min(match, sum(sum(cv2.absdiff(e2, e1[i:i+len(e2)])*0.1)))
+        return match*100.0/len(e2)
     else:
-        match = 10000*len(e1)
+        match = 50000
         for i in range(0-dif):
-            match = min(match, sum(sum(cv2.absdiff(e1, e2[i:i+len(e1)])*1.0)))
-        return match*1.0/len(e1)
+            match = min(match, sum(sum(cv2.absdiff(e1, e2[i:i+len(e1)])*0.1)))
+        return match*100.0/len(e1)
     
 def getedge(piece, edge):
-    if edge == "W":
+    if type(edge) == type(5):
+        sides = ["N","E","S","W"]
+        side = sides[edge%4]
+    else:
+        side = edge
+    if side == "W":
         return(piece[:,0, ])
-    if edge == "N":
+    elif side == "N":
         return(piece[0,:, ])
-    if edge == "E":
+    elif side == "E":
         return(piece[:,-1, ])
-    if edge == "S":
+    elif side == "S":
         return(piece[-1,:, ])
+
     
 def showMatch(pieces, matchid): 
     i,j,k,l = matchid
     im = rotatePiece(pieces[i], k)
     im2 = rotatePiece(pieces[j], (l+2)%4)
     im = np.append(im2, im, axis = 0)
-    cv2.imshow('piece'+str(i), im)
+    cv2.imshow('piece'+str(i)+str(j), im)
 
 def rotatePiece(piece, i):
     if i == 0: #North side
@@ -78,9 +84,8 @@ def rotatePiece(piece, i):
         
     
 def getBestMatch(pieces):
-    
     sides = ["N","E","S","W"]
-    bestmatch = 10000
+    bestmatch = 50000
     matchid = [0,0,0,0]
     matches = np.empty((len(pieces), len(pieces), len(sides), len(sides)), dtype = 'uint16')
     for i in range(len(pieces)):
@@ -91,41 +96,67 @@ def getBestMatch(pieces):
                     for l in range(len(sides)):
                         match = int(getMatch(e1, getedge(pieces[j],sides[l])))
                         matches[i,j,k,l]= match
+                        matches[j,i,l,k]= match
                         if match < bestmatch:
                             bestmatch = match
-                            showPiece(pieces, i, j)
-                            print match
                             matchid = [i,j,k,l]
-                            showMatch(pieces, matchid)
-                            print(matchid)
-                            cv2.waitKey()
-                            cv2.destroyAllWindows()
+                            rev = [j,i,l,k]
+#                            cv2.waitKey()
+#                            cv2.destroyAllWindows()
                 else:
-                    matches[i,j,k]= list(10000 for l in range(len(sides)))
+                    matches[i,j,k]= list(50000 for l in range(len(sides)))
+    print bestmatch
+    print(matchid)
+    showMatch(pieces, matchid)
+    showMatch(pieces, rev)
+    print matches[matchid[1], matchid[0], matchid[3], matchid[2]]
+#    print matches[rev]
     return matches, matchid
             
-def getNeighbour(pieceNmr, sideNmr, matches):
-    sides = ["N","E","S","W"]
-    bestmatch = 10000
+def getNeighbour(pieces, matches, pieceNmr, sideNmr):
     matchid = [0,0,0,0]
-    e1 = getedge(pieces[pieceNmr],sides[sideNmr])
+    bestmatch = 50000
     for i in range(len(matches)):
-        for j in range(len(sides)):
-            match = int(getMatch(getedge(pieces[i],sides[j]), e1))
+        if (not i == pieceNmr):
+            for j in range(len(matches[0,0,0])):
+                match = matches[i, pieceNmr, j, sideNmr]
+                if match < bestmatch:
+                    bestmatch = match
+                    matchid = [i, pieceNmr, j, sideNmr]
+                    showMatch(pieces, matchid)
+
+                    cv2.waitKey()
+                    cv2.destroyAllWindows()
+    print bestmatch
+    print matchid
+    return matchid
+
+def getLine(pieces, matches, matchid):
+    length = 2
+    new_edge = matchid[0, 2]
+    old_edge = matchid[1, 3]
+    old_match = getNeighbour(matches, old_edge)
+    line = list(0 for i in range(np.floor(np.sqrt(len(matches)))))
+    line[0] = matchid
+#    while length < len(line):
+        
+    
 
 puzzleType="tiles"
 puzzleArrangement="rotated"
 puzzleSize="5x5"
 puzzleNumber="04"        
 puzzle = cv2.imread('images/'+puzzleType+'/'+puzzleType+'_'+puzzleArrangement+'/'+puzzleType+'_'+puzzleArrangement+'_'+puzzleSize+'_'+puzzleNumber+'.png')
-
 pieces = dividePuzzle(puzzle, 25)
 
-
 sides = ["N","E","S","W"]
-bestmatch = float("inf")
 matches, matchid = getBestMatch(pieces)
-
+match2 = getNeighbour(pieces, matches, matchid[0], (matchid[2]+2)%4)
+match3 = getNeighbour(pieces, matches, matchid[1], (matchid[3]+2)%4)
+showMatch(pieces, match2)
+showMatch(pieces, match3)
+cv2.waitKey()
+cv2.destroyAllWindows()
 
 #cv2.imshow('im', puzzle)
 #cv2.waitKey()
