ec1fedd92fc7b5ffe2eaeac655cc9424bd8b9c7b
LovelySanta <laurens.scheldeman@gmail.com>
2017-12-05 16:49:15 +0100

    some additions

--- Puzzle.py
+++ Puzzle.py
@@ -168,6 +168,18 @@ class Puzzle:
 
 
 
+    def showMatch(self, matchId):
+        i,j,k,l = matchid
+        # First matching image
+        piece1 = rotatePiece(self.pieces[i], k)
+        # Second matching image
+        piece2 = rotatePiece(self.pieces[j], (l+2)%4)
+
+        match = np.append(piece2, piece1, axis = 0)
+        cv2.imshow('pieces'+str(i)+' and '+str(j), match)
+
+
+
     def rotateRansac(self, contour):
         T_i = len(contour)/10
         teller = 0
@@ -242,62 +254,63 @@ class Puzzle:
         return hoek
 
 
+
     def getMatchingValue(self, edge1, edge2):
         dif = len(edge1)-len(edge2)
-        ### If two pieces are too different in length, they won't fit
-        if abs(dif) > 5:
-            return 10000
 
-        ### If perfect length just return the difference
+        # If dimensions mismatch too much (rectangle), it won't match at all
+        if abs(dif) > 5:
+            return 50000
+
+        # Perfect match
         elif dif == 0:
             #cv2.imshow('diff', abs(cv2.absdiff(edge2, edge1))[:,0])
-            return sum(sum(abs(cv2.absdiff(edge2, edge1)*1.0)))*256.0/len(edge1)
+            return sum(sum(abs(cv2.absdiff(edge2, edge1)*0.1)))*100.0/len(edge1)
 
-        ### If difference is <= 5, then give the best mach for all possible matchings
+        # Small mismatch (rotation error)
         elif dif > 0:
-            match = 10000*len(edge2)
+            match = 50000
             for i in range(dif):
-                match = min(match, sum(sum(cv2.absdiff(edge2, edge1[i:i+len(edge2)])*1.0)))
-            return match*1.0/len(edge2)
+                match = min(match, sum(sum(cv2.absdiff(edge2, edge1[i:i+len(edge2)])*0.1)))
+            return match*100.0/len(edge2)
         else:
-            match = 10000*len(edge1)
+            match = 50000
             for i in range(0-dif):
-                match = min(match, sum(sum(cv2.absdiff(edge1, edge2[i:i+len(edge1)])*1.0)))
-            return match*1.0/len(edge1)
+                match = min(match, sum(sum(cv2.absdiff(edge1, edge2[i:i+len(edge1)])*0.1)))
+            return match*100.0/len(edge1)
+
+
 
     def getBestMatch(self):
-        bestmatch = float('Inf')
+        bestmatch = 50000
         matchid = [0,0,0,0]
 
         numberOfPieces = len(self.puzzlePieces)
         numberOfSides = len(self.sides)
         matches = np.empty((numberOfPieces, numberOfPieces, numberOfSides, numberOfSides), dtype = 'float64')
 
-        # Iterate over all pieces to match all pieces with all pieces on all sides
+        # Iterate over all edges of all pieces...
         for i in range(numberOfPieces):
-            # We only have to match A to B as B to A has same value
-            for j in range(i, numberOfPieces):
-                # All sides...
-                for k in range(numberOfSides):
-                    for l in range(numberOfSides):
-                        # If it matches itself, we say it's not a match for sure
-                        if i == j:
-                            match = float('Inf')
-                        else:
+            for k in range(numberOfSides):
+                edge1 = self.puzzlePieces[i].getEdge(self.sides[k])
+                # Match this side to all other sides of other pieces.
+                for j in range(numberOfPieces):
+                    if not (i == j):
+                        for l in range(numberOfSides):
+                            # Get the mathcing value
                             match = int(self.getMatchingValue(
-                                self.puzzlePieces[i].getEdge(self.sides[k]),
+                                edge1,
                                 self.puzzlePieces[j].getEdge(self.sides[l])  ))
-                        matches[i,j,k,l] = match
-                        matches[j,i,l,k] = match
+                            matches[i,j,k,l] = match
+                            matches[j,i,l,k] = match
 
-                        # Check if the match is the best match up till now
-                        if match < bestmatch:
-                            bestmatch = match
-                            matchid = [i,j,k,l]
-
-                            print bestmatch
-                            self.puzzlePieces[i].showPuzzlePiece(windowName="Piece 1", addWaitKey=False)
-                            self.puzzlePieces[j].showPuzzlePiece(windowName="Piece 2")
+                            # Check if the match is the best match up till now
+                            if match < bestmatch:
+                                bestmatch = match
+                                matchid = [i,j,k,l]
+                                rev = [j,i,l,k]
+                    else:
+                        matches[i,j,k]= list(50000 for l in range(len(sides)))
 
         # Return all matching values we calculated and the index of the two best matchings
         return matches, matchid
--- PuzzlePiece.py
+++ PuzzlePiece.py
@@ -4,6 +4,8 @@ import cv2
 class PuzzlePiece:
 
     def __init__(self, piece):
+        self.sides = ["N","E","W","S"]
+
         self.piece = piece
         self.h, self.w, self.channels = self.piece.shape
 
@@ -34,19 +36,40 @@ class PuzzlePiece:
         return mask
 
     def getEdge(self, edge):
+        # Match integer to string
+        if type(edge) == type(5):
+            side = self.sides[edge%4]
+        else:
+            side = edge
+
+        # Get the edge
         if edge == "W":
             return(self.piece[:,0, ])
-
         elif edge == "N":
             return(self.piece[0,:, ])
-
         elif edge == "E":
             return(self.piece[:,-1, ])
-
         elif edge == "S":
             return(self.piece[-1,:, ])
-
         else:
             return None
+
+    def mirrorPiece(self, axis, piece=self.piece):
+        if axis == 'H':
+            return cv2.flip(self.piece, 0)
+        elif axis == 'V':
+            return cv2.flip(self.piece, 1)
+        elif axis == 'D':
+            return = cv2.transpose(self.piece)
+
+    def rotatePiece(self, rotation):
+            if rotation % 4 == 0: # North side
+                return self.piece
+            elif rotation % 4 == 1: # East side
+                return self.mirror('H', self.mirror('D'))
+            elif rotation % 4 == 2: # South side
+                return self.mirror('V', self.mirror('H'))
+            elif rotation % 4 == 3: # West side
+                return self.mirror('D', self.mirror('H'))
 
 ### End Of File ###
