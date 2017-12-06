c683978d719abea289b691c00b1cdb9bb0824daa
Jarno21 <Jagrenson@gmail.com>
2017-12-06 20:05:43 +0100

    2D matching

--- testings/getMatchingValues.py
+++ testings/getMatchingValues.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Wed Dec 06 16:05:26 2017
+
+@author: jarno
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Tue Nov 28 14:43:09 2017
 
 @author: jarno
@@ -36,18 +43,17 @@ def getMatch(e1, e2):
         return 50000
     elif dif == 0:
         #cv2.imshow('diff', abs(cv2.absdiff(e2, e1))[:,0])
-
-        return sum(sum(abs(cv2.absdiff(e2, e1)*0.1)))*100.0/len(e1)
+        return sum(sum(abs(cv2.absdiff(e2, e1)**1.5*0.1)))*10.0/len(e1)
     elif dif > 0:
         match = 50000
         for i in range(dif):
             match = min(match, sum(sum(cv2.absdiff(e2, e1[i:i+len(e2)])*0.1)))
-        return match*100.0/len(e2)
+        return match*10.0/len(e2)**2
     else:
         match = 50000
         for i in range(0-dif):
             match = min(match, sum(sum(cv2.absdiff(e1, e2[i:i+len(e1)])*0.1)))
-        return match*100.0/len(e1)
+        return match*10.0/len(e1)
 
 def getedge(piece, edge):
     if type(edge) == type(5):
@@ -91,12 +97,12 @@ def getBestMatch(pieces):
     for i in range(len(pieces)):
         for k in range(len(sides)):
             e1 = getedge(pieces[i],sides[k])
-            for j in range(i, len(pieces)):
+            for j in range( len(pieces)):
                 if not(i == j):
                     for l in range(len(sides)):
                         match = int(getMatch(e1, getedge(pieces[j],sides[l])))
                         matches[i,j,k,l]= match
-                        matches[j,i,l,k]= match
+#                        matches[j,i,l,k]= match
                         if match < bestmatch:
                             bestmatch = match
                             matchid = [i,j,k,l]
@@ -137,7 +143,7 @@ def getNeighbourEx( matches, edgeId, exclusions):
     matchid = [0,0,0,0]
     bestmatch = 50000
     for i in range(len(matches)):
-        if (not i == pieceNmr) and i not in exclusions:
+        if i not in exclusions:
             for j in range(len(matches[0,0,0])):
                 match = matches[i, pieceNmr, j, sideNmr%4]
                 if match < bestmatch:
@@ -208,6 +214,200 @@ def combinePuzzle(pieces, placedPieces):
     cv2.waitKey()
     cv2.destroyAllWindows()
     return image
+
+# DISCLAIMER! THE EDGES SHOULD BE SENT CLOCKWISE
+# EXAMPLE: FIRST EDGE IS THE RIGHT EDGE OF A PIECE, SECOND ONE IS THE BOTTOM ONE
+# IF THIS IS NOT FOLLOWED, MISMATCHES MAY AND WILL HAPPEN
+# THE REFERENCE FOR THE MATCH WILL BE DIRECTED TOWARDS THE FIRST NUMBER
+def getNeighbour2D(matches, edgeIDs, exclusions): 
+    pieceNmr1, sideNmr1 = edgeIDs[0]
+    pieceNmr2, sideNmr2 = edgeIDs[1]
+    matchid = matchid2 = [-1,0,0,0]
+    bestmatch = 5000000
+    for i in range(len(matches)):
+        if (not i in exclusions):
+            for j in range(len(matches[0,0,0])):
+                match1 = matches[i, pieceNmr1, j, sideNmr1%4]
+                match2 = matches[i, pieceNmr2, (j+1)%4, (sideNmr2+1)%4]
+                match = match1**2+match2**2
+                if match < bestmatch:
+                    bestmatch = match
+                    matchid = [i, pieceNmr1, j, sideNmr1%4]
+                    matchid2 = [i, pieceNmr2, (j+1)%4, (sideNmr2+1)%4]
+#                    print match, match1, match2
+#    showMatch(pieces, matchid)
+#    showMatch(pieces, matchid2)
+
+#                    cv2.waitKey()
+#                    cv2.destroyAllWindows()
+#    print bestmatch
+#    print matchid
+    return matchid
+
+def showsolvedPuzzle(pieces, placedPieces):
+    h,w,chan = np.shape(pieces[0])
+    count = int(np.sqrt(len(pieces)))
+    image = np.zeros((h*count, w*count, chan), dtype = 'uint8')
+    for i in range(len(placedPieces)):
+        for j in range(len(placedPieces[0])):
+            piece, rot = placedPieces[i][j]
+            if not piece == -1:
+                im = rotatePiece(pieces[piece], rot)
+                x = i*h
+                y = j*w
+                image[x:x+h, y:y+w] = im
+    cv2.imshow('puzzle', image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    return image                
+            
+
+def fillBlock(matches, placedPieces, used, matchid, pieces):
+    x, y, n, r, d = matchid
+    newLoc = [x,y]
+    newPieces = [0]
+    newPieces[0] = [x,y,n,r]
+    used_t = used
+    if d == 0 or d == 4:
+        oldLoc = [x+1, y-1]
+        OL2 = [x+1, y+1]
+        dir2 = [0,1]
+        dir1 = [0,-1]
+
+    elif d == 3:
+        oldLoc = [x+1, y+1]
+        OL2 = [x-1, y+1]
+        dir2 = [-1,0]
+        dir1 = [1,0]
+        
+    elif d == 2:
+        oldLoc = [x-1, y+1]
+        OL2 = [x-1, y-1]
+        dir2 = [0,-1]
+        dir1 = [0,1]
+        
+    else: 
+        oldLoc = [x-1, y-1]
+        OL2 = [x+1, y-1]
+        dir2 = [1,0]
+        dir1 = [-1,0]
+        
+    print oldLoc
+    while -1 < oldLoc[0] < len(placedPieces) and -1 < oldLoc[1] < len(placedPieces) and placedPieces[tuple(oldLoc)][0] > -1:
+        oldPiece = np.add(placedPieces[tuple(oldLoc)], [0,d-1])
+        newPiece = np.add(placedPieces[tuple(newLoc)], [0,d-1])
+        neighbour = getNeighbour2D(matches, [newPiece, oldPiece], used)
+        oldLoc = np.add(oldLoc, dir1)
+        newLoc = np.add(newLoc, dir1)
+        piece = np.append(newLoc, (neighbour[0], neighbour[2]+d*3+3))
+        newPieces.append(piece)
+#        print piece
+        placedPieces[tuple(newLoc)] = [neighbour[0],(neighbour[2]+d*3+3)%4]
+        used.append(neighbour[0])
+#        showsolvedPuzzle(pieces, placedPieces)
+#        print oldLoc
+    oldLoc = OL2
+    newLoc = [x,y]
+    used = used_t
+    print oldLoc
+    while -1 < oldLoc[0] < len(placedPieces) and -1 < oldLoc[1] < len(placedPieces) and placedPieces[tuple(oldLoc)][0] > -1:
+        oldPiece = np.add(placedPieces[tuple(oldLoc)], [0,d])
+        newPiece = np.add(placedPieces[tuple(newLoc)], [0,d])
+        neighbour = getNeighbour2D(matches, [oldPiece, newPiece], used)
+        oldLoc = np.add(oldLoc, dir2)
+        newLoc = np.add(newLoc, dir2)
+        piece = np.append(newLoc, (neighbour[0], neighbour[2]+d*3+2))
+        newPieces.append(piece)
+#        print piece
+        placedPieces[tuple(newLoc)] = [neighbour[0],(neighbour[2]+d*3+2)%4]
+        used.append(neighbour[0])
+#        showsolvedPuzzle(pieces, placedPieces)
+#        print oldLoc
+        
+    return newPieces
+    
+def expandBlock(matches, placedPieces, used):
+    #check for max width
+    i = j = len(placedPieces[0])-1
+    bestMatch = 50000
+    matchid = [-1,-1,-1,-1,-1]
+    while placedPieces[i][0][0] == -1:
+        i = i-1
+    while placedPieces[0][j][0] == -1:
+        j = j-1
+    i = i
+    j = j
+    print i,j
+    if not j >= len(placedPieces)-1:
+        for k in range(i+1):
+#            print "k in i:"
+#            print k
+            Mid = getNeighbourEx(matches, [placedPieces[k][0][0], (placedPieces[k][0][1]-1)%4], used)
+            newMatch = matches[tuple(Mid)]
+            if newMatch < bestMatch:
+                bestMatch = newMatch
+                matchid = [k,-1, Mid[0], (Mid[2]-1)%4, 3]
+                BM = Mid
+            Mid = getNeighbourEx(matches, [placedPieces[k][j][0], (placedPieces[k][j][1]+1)%4], used)
+            newMatch = matches[tuple(Mid)]
+            if newMatch < bestMatch:
+                bestMatch = newMatch
+                matchid = [k,j+1, Mid[0], (Mid[2]+1)%4, 1] 
+                BM = Mid
+                
+                
+    if not i >= len(placedPieces)-1:
+        for k in range(j+1):
+#            print "k:"
+#            print k, j
+            Mid = getNeighbourEx(matches, [placedPieces[0][k][0], placedPieces[0][k][1]], used)
+            newMatch = matches[tuple(Mid)]
+            if newMatch < bestMatch:
+                bestMatch = newMatch
+                matchid = [-1,k, Mid[0], (Mid[2]-2)%4, 0]
+                BM = Mid
+            Mid = getNeighbourEx(matches, [placedPieces[i][k][0], (placedPieces[i][k][1]+2)%4], used)
+            newMatch = matches[tuple(Mid)]
+            if newMatch < bestMatch:
+                bestMatch = newMatch
+                matchid = [i+1,k, Mid[0], Mid[2], 2] 
+                BM = Mid
+    return matchid
+                  
+
+def solverMK2(pieces, matches, matchid):
+    
+    i,j,k,l = matchid
+    width = int(np.sqrt(len(pieces)))
+    placedPieces = np.full((width,width,2), -1, dtype = 'int8')
+    placedPieces[0][0] = [i,(k-2)%4]
+    placedPieces[1][0] = [j,l]
+    used = [i,j]
+    while len(used) < 25:
+        matchid = expandBlock(matches, placedPieces, used)
+        if matchid[0] == -1:
+            placedPieces = np.append(np.full((1,len(placedPieces), 2), -1, dtype = 'int8'),placedPieces[0:-1], axis = 0)
+            matchid[0] = 0
+        elif matchid[1] == -1:
+            placedPieces = np.append(np.full((len(placedPieces), 1, 2), -1, dtype = 'int8'),placedPieces[:,0:-1], axis = 1)
+            matchid[1] = 0
+        print matchid
+        showsolvedPuzzle(pieces, placedPieces)
+        placedPieces[matchid[0]][matchid[1]] = [matchid[2], matchid[3]]
+        showsolvedPuzzle(pieces, placedPieces)
+        used.append(matchid[2])
+        matchid = fillBlock(matches, placedPieces, used,  matchid, pieces)
+        for piece in range(len(matchid)):
+            print matchid[piece]
+            placedPieces[matchid[piece][0]][matchid[piece][1]] = [matchid[piece][2], matchid[piece][3]]
+            used.append(matchid[piece][2])
+#        placedPieces[matchid[0][0]][matchid[0][1]] = [matchid[0][2], matchid[0][3]]
+        
+    showsolvedPuzzle(pieces, placedPieces)
+    return placedPieces
+
+
+
 
 def puzzleSolver(pieces, matches, matchid):
 
@@ -285,7 +485,7 @@ puzzleType="tiles"
 puzzleArrangement="rotated"
 puzzleSize="4x4"
 puzzleNumber="04"
-puzzle = cv2.imread(puzzleType+'/'+puzzleType+'_'+puzzleArrangement+'/'+puzzleType+'_'+puzzleArrangement+'_'+puzzleSize+'_'+puzzleNumber+'.png')
+puzzle = cv2.imread(puzzleType+'_'+puzzleArrangement+'_'+puzzleSize+'_'+puzzleNumber+'.png')
 pieces = dividePuzzle(puzzle, 16)
 
 sides = ["N","E","S","W"]
@@ -295,9 +495,16 @@ matches, matchid = getBestMatch(pieces)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-x, minx = puzzleSolver(pieces, matches, matchid)
-im = combinePuzzle(pieces, x)
 
-cv2.imshow('im', im)
+#x, minx = puzzleSolver(pieces, matches, matchid)
+y = solverMK2(pieces, matches, matchid)
+showsolvedPuzzle(pieces, y)
+#im = combinePuzzle(pieces, x)
+trial = sum(sum(abs(cv2.absdiff(getedge(pieces[9], 2), getedge(pieces[10],0))*0.1)))*10.0/len(pieces[0])
+test2 = sum(abs(cv2.absdiff(getedge(pieces[9], 2), getedge(pieces[10],0))*0.1))*10.0/len(pieces[0])
+test3 = abs(cv2.absdiff(getedge(pieces[9], 2), getedge(pieces[10],0))*0.1)*10.0
+test4 = abs(cv2.absdiff(getedge(pieces[15], 0), getedge(pieces[12],0))*0.1)*10.0
+#cv2.imshow('im', im)
 cv2.waitKey()
 cv2.destroyAllWindows()
+
